d01183264cf2a544287122d714ba14e0f5a4ada3
Aditya joshi <adityajoshi1602@gmail.com>
2026-03-27 15:48:29 +0530

    added cors and connected with imagekit

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+const cors = require('cors')
 const express = require('express')
 const multer = require('multer')
 const uploadfiles = require('./services/storage.service')
+const postmodel = require('./models/post.model')
 const app = express()
+
+
+app.use(cors())
 app.use(express.json())
 
 const upload = multer({ storage: multer.memoryStorage() })
@@ -15,13 +20,32 @@ app.post('/create', upload.single('image'), async (req, res) => {
 
         console.log(result)
 
+        await postmodel.create({
+            image: result.url,
+            caption: req.body.caption
+        })
         res.status(200).json({
             msg: 'uploaded',
-            url: result.url
+            image: result.url,
+            caption: req.body.caption
         })
+    }
+    catch (err) {
+        console.error(err)
+        res.status(500).json({ msg: err.message })
+    }
+})
 
+
+app.get('/posts', async (req, res) => {
+    try {
+        const posts = await postmodel.find()
+
+        res.status(200).json(posts)
     } catch (err) {
+        console.error(err)
         res.status(500).json({ msg: 'error' })
     }
 })
+
 module.exports = app
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 require('dotenv').config()
-
 const app = require('./app')
 const db = require('./db/db')
-
 db()
 
 app.listen(3000, () => {
